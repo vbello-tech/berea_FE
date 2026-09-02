5ca226a4e2e0b272badf6dcea706a138630ad106
Vbello-Tech <69384115+vbello-tech@users.noreply.github.com>
2026-09-02 13:44:47 +0100

    chore: change book selection style

--- src/components/PassageFinder.vue
+++ src/components/PassageFinder.vue
@@ -18,8 +18,8 @@ const form = reactive({
 const OLD_TESTAMENT = new Set([
   'Genesis', 'Exodus', 'Leviticus', 'Numbers', 'Deuteronomy', 'Joshua', 'Judges',
   'Ruth', '1 Samuel', '2 Samuel', '1 Kings', '2 Kings', '1 Chronicles', '2 Chronicles',
-  'Ezra', 'Nehemiah', 'Esther', 'Job', 'Psalms', 'Proverbs', 'Ecclesiastes',
-  'Song of Solomon', 'Isaiah', 'Jeremiah', 'Lamentations', 'Ezekiel', 'Daniel',
+  'Ezra', 'Nehemiah', 'Esther', 'Job', 'Psalm', 'Proverbs', 'Ecclesiastes',
+  'Song Of Solomon', 'Isaiah', 'Jeremiah', 'Lamentations', 'Ezekiel', 'Daniel',
   'Hosea', 'Joel', 'Amos', 'Obadiah', 'Jonah', 'Micah', 'Nahum', 'Habakkuk',
   'Zephaniah', 'Haggai', 'Zechariah', 'Malachi',
 ]);
